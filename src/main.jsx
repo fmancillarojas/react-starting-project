3102ace05748adb8034745423fe81c5c275bb52b
Francisco Mancilla <fmancillarojas@academic.rrc.ca>
2024-05-08 11:56:36 -0500

    Added dynamic routes, posts details

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Posts from './routes/Posts';
+import Posts, { loader as postsLoader } from './routes/Posts';
 import RootLayout from './routes/RootLayout';
-import NewPost from './routes/NewPost';
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails';
+
 import './index.css';
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [
-    { path: '/', element: <Posts />, children: [
-      { path: '/create-post', element: <NewPost /> },
-    ] },
-  ]},
+  {
+    path: '/', element: <RootLayout />, children: [
+      {
+        path: '/', element: <Posts />, loader: postsLoader, children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails />, loader: postDetailsLoader }
+        ]
+      },
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
